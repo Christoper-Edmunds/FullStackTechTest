9c8ae219df8375a001b63d5c841d2866cd594a53
Chris <crristopher@gmail.com>
2024-09-02 15:56:45 +0100

    a little cleanup and a a quick test to make sure the import is working. Will seperate model out at the end. Next is data saving, then validation, then UI

--- DAL/UploadRepository.cs
+++ DAL/UploadRepository.cs
@@ -16,10 +16,15 @@ public class UploadRepository : IUploadRepository
         //"C:\Users\crris\source\repos\FullStackTechTest\data.json"
         string fileName = @"C:\Users\crris\source\repos\FullStackTechTest\data.json";
         using FileStream openStream = File.OpenRead(fileName);
-        ImportData? importData =
-            await JsonSerializer.DeserializeAsync<ImportData>(openStream);
+        //ImportData? importData =
+        //    await JsonSerializer.DeserializeAsync<ImportData>(openStream);
 
-        //Console.WriteLine($"firstName: {importData?.firstName}");
+        //ImportData[] FromJson(string fileName) => JsonSerializer.Deserialize<ImportData[]>(fileName);
+        ImportData[] importData = JsonSerializer.Deserialize<ImportData[]>(openStream);
+
+
+        //foreach(var individualperson in importData.)
+        //Console.WriteLine($"firstName: {importData?.FirstName}");
         //Console.WriteLine($"lastName: {importData?.lastName}");
         //Console.WriteLine($"GMC: {importData?.GMC}");
         //Console.WriteLine($"line1: {importData?.line1}");
@@ -30,33 +35,33 @@ public class UploadRepository : IUploadRepository
     }
 
 
-public partial class ImportData
-{
-    [JsonPropertyName("firstName")]
-    public string FirstName { get; set; }
+    public partial class ImportData
+    {
+        [JsonPropertyName("firstName")]
+        public string FirstName { get; set; }
 
-    [JsonPropertyName("lastName")]
-    public string LastName { get; set; }
+        [JsonPropertyName("lastName")]
+        public string LastName { get; set; }
 
-    [JsonPropertyName("GMC")]
-    public long Gmc { get; set; }
+        [JsonPropertyName("GMC")]
+        public long Gmc { get; set; }
 
-    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
-    [JsonPropertyName("address")]
-    public Address[] Address { get; set; }
-}
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+        [JsonPropertyName("address")]
+        public Address[] Address { get; set; }
+    }
 
-public partial class Address
-{
-    [JsonPropertyName("line1")]
-    public string Line1 { get; set; }
+    public partial class Address
+    {
+        [JsonPropertyName("line1")]
+        public string Line1 { get; set; }
 
-    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
-    [JsonPropertyName("city")]
-    public string City { get; set; }
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+        [JsonPropertyName("city")]
+        public string City { get; set; }
 
-    [JsonPropertyName("postcode")]
-    public string Postcode { get; set; }
-}
+        [JsonPropertyName("postcode")]
+        public string Postcode { get; set; }
+    }
 
 }
