63e7a644d32c691fb1cb7dd61c75a151eeead82d
Chris <crristopher@gmail.com>
2024-09-16 23:35:56 +0100

    Built out DAL for accessing the specialities table. The model is not currently implimented and is just there as a placeholder

---
+++ DAL/ISpecialityRepository.cs
@@ -0,0 +1,10 @@
+﻿using Models;
+
+namespace DAL
+{
+    public interface ISpecialityRepository
+    {
+        Task<List<Specialities>> ListAllSpecialitiesAsync();
+        Task<List<string>> ListAllSpecialitiesByIdAsync(int personId);
+    }
+}
---
+++ DAL/SpecialitiesRepository.cs
@@ -0,0 +1,78 @@
+﻿using Models;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class SpecialitiesRepository : ISpecialityRepository
+    {
+        public async Task<List<Specialities>> ListAllSpecialitiesAsync()
+        {
+            var specialitiesList = new List<Specialities>();
+
+            var sql = new StringBuilder();
+            sql.AppendLine("SELECT * FROM specialities");
+
+            await using (var connection = new MySqlConnection(Config.DbConnectionString))
+            {
+                await connection.OpenAsync();
+
+                var command = new MySqlCommand(sql.ToString(), connection);
+
+                var reader = await command.ExecuteReaderAsync();
+                while (await reader.ReadAsync())
+                {
+                    specialitiesList.Add(PopulateSpeciality(reader));
+                }
+            }
+
+            return specialitiesList;
+        }
+
+
+        public async Task<List<string>> ListAllSpecialitiesByIdAsync(int personId)
+        {
+            var specialitiesList = new List<string>();
+
+            await using (var connection = new MySqlConnection(Config.DbConnectionString))
+            {
+                await connection.OpenAsync();
+
+                const string listSpecialitiesByIdSQL = @"
+                    SELECT Specialities.SpecialityName
+                    FROM Specialities
+                    JOIN PeopleSpecialitiesLinkTable ON Specialities.Id = PeopleSpecialitiesLinkTable.SpecialityID
+                    JOIN People ON PeopleSpecialitiesLinkTable.PersonID = People.Id
+                    WHERE People.Id = @personId;";
+
+                await using (var command = new MySqlCommand(listSpecialitiesByIdSQL, connection))
+                {
+                    command.Parameters.AddWithValue("@personId", personId);
+
+                    var reader = await command.ExecuteReaderAsync();
+                    while (await reader.ReadAsync())
+                    {
+                        specialitiesList.Add(reader.GetString("SpecialityName"));
+                    }
+                }
+            }
+
+            return specialitiesList;
+        }
+
+        private Specialities PopulateSpeciality(IDataRecord data)
+        {
+            var speciality = new Specialities
+            {
+                Id = Convert.ToInt32(data["Id"]),
+                SpecialityName = data["SpecialityName"].ToString(),
+            };
+            return speciality;
+        }
+    }
+}
